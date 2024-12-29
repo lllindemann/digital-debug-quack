5c0da52c7b13866775c15d0ab3b6d8a9bfc3c6ef
llindemann <leonie.lindemann.ll@gmail.com>
2024-12-29 23:37:02 +0100

    feat: top navbar

---
+++ digital-debug-quack/components/header.tsx
@@ -0,0 +1,14 @@
+"use client"
+import MainNav from './main-nav';
+import MobileNav from './mobile-nav';
+
+export default function SiteHeader() {
+  return (
+    <header className="w-full border-b">
+      <div className="flex h-14 items-center px-4">
+        <MainNav />
+        <MobileNav />
+      </div>
+    </header>
+  );
+}
---
+++ digital-debug-quack/components/main-nav.tsx
@@ -0,0 +1,16 @@
+"use client"
+import { Button } from './ui/button';
+
+const mainNavItems = ['A', 'B', 'C'];
+
+export default function MainNav() {
+  return (
+    <div className="mr-4 hidden gap-2 md:flex">
+      {mainNavItems.map((item, index) => (
+        <Button key={index} variant="link">
+          {item}
+        </Button>
+      ))}
+    </div>
+  );
+}
